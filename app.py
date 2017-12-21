39610a91b1310aff7e7272d9016ef12f4026be39
Sajib Goswami <sabujgoswami_cou@yahoo.com>
2017-12-22 01:13:04 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+
+
+#requests are objects that flask handles (get set post, etc)
+from flask import Flask, render_template,request
+#scientific computing library for saving, reading, and resizing images
+from scipy.misc import imsave, imread, imresize
+#for matrix math
+import numpy as np
+#for importing our keras model
+import keras.models
+#for regular expressions, saves time dealing with string data
+import re
+
+#system level operations (like loading files)
+import sys 
+#for reading operating system data
+import os
+#tell our app where our saved model is
+sys.path.append(os.path.abspath("./model"))
+from load import * 
+#initalize our flask app
+app = Flask(__name__)
+#global vars for easy reusability
+global model, graph
+#initialize these variables
+model, graph = init()
+
+#decoding an image from base64 into raw representation
+def convertImage(imgData):
+	imgstr = re.search(r'base64,(.*)',imgData).group(1)
+	#print(imgstr)
+	with open('output.png','wb') as output:
+		output.write(imgstr.decode('base64'))
+	
+
+@app.route('/')
+def index():
+	#initModel()
+	#render out pre-built HTML file right on the index page
+	return render_template("index.html")
+
+@app.route('/predict/',methods=['GET','POST'])
+def predict():
+	#whenever the predict method is called, we're going
+	#to input the user drawn character as an image into the model
+	#perform inference, and return the classification
+	#get the raw data format of the image
+	imgData = request.get_data()
+	#encode it into a suitable format
+	convertImage(imgData)
+	
+	#read the image into memory
+	x = imread('output.png',mode='L')
+	#compute a bit-wise inversion so black becomes white and vice versa
+	x = np.invert(x)
+	#make it the right size
+	x = imresize(x,(128,128))
+	#imshow(x)
+	#convert to a 4D tensor to feed into our model
+	x = x.reshape(None,1,128,128)
+	print ("debug2")
+	#in our computation graph
+	with graph.as_default():
+		#perform the prediction
+		out = model.predict(x)
+		print(out)
+		print(np.argmax(out,axis=1))
+		print ("debug3")
+		#convert the response to a string
+		response = np.array_str(np.argmax(out,axis=1))
+		return response	
+	
+
+if __name__ == "__main__":
+	#decide what port to run the app in
+	port = int(os.environ.get('PORT', 5000))
+	#run the app locally on the givn port
+	app.run(host='0.0.0.0', port=port)
+	#optional if we want to run in debugging mode
+	#app.run(debug=True)
